b2472fadd455fbbafac66329a7812c3d63cbe7c3
felipricardo <felipebarbosaok@gmail.com>
2023-09-05 14:39:02 -0300

    up

--- ThreadForm/Form1.cs
+++ ThreadForm/Form1.cs
@@ -4,7 +4,9 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Reflection;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -13,10 +15,14 @@ namespace ThreadForm
     public partial class Form1 : Form
     {
         private delegate void AtualizarControle(Control controle, string propridade, object valor);
+
+        Thread t;
         
         public Form1()
         {
             InitializeComponent();
+            t = new Thread(new ThreadStart(Tarefa));
+            t.IsBackground = true;
         }
 
         private void btnPrincipal_Click(object sender, EventArgs e)
@@ -26,9 +32,37 @@ namespace ThreadForm
 
         private void btnContador_Click(object sender, EventArgs e)
         {
+            
+        }
+
+        private void Tarefa()
+        {
             while (true)
             {
-                lblResultado.Text = DateTime.Now.Second.ToString();
+                //lblResultado.Text = DateTime.Now.Second.ToString();
+                DefinirValorPropriedade(lblResultado, "Text", DateTime.Now.Second.ToString());
+            }
+        }
+
+        private void DefinirValorPropriedade(Control controle, string propriedade, object valor)
+        {
+            if (controle.InvokeRequired)
+            {
+                AtualizarControle d = new AtualizarControle(DefinirValorPropriedade);
+                controle.Invoke(d, new object[] {controle, propriedade, valor});
+            }
+            else
+            {
+                Type t = controle.GetType();
+                PropertyInfo[] props = t.GetProperties();
+
+                foreach (PropertyInfo p in props)
+                {
+                    if (p.Name.ToUpper() == propriedade.ToUpper())
+                    {
+                        p.SetValue(controle, valor, null);
+                    }
+                }
             }
         }
     }
