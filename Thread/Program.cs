a8109cfe1c15a583a4be1b2c8c53b2c510d2f468
felipricardo <felipebarbosaok@gmail.com>
2023-09-05 12:16:57 -0300

    up

---
+++ Thread/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Thread
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Tarefa();
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Principal");
+            }
+
+            Console.ReadKey();
+        }
+
+        static void Tarefa()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Tarefa Executada.");
+            }
+        }
+    }
+}
