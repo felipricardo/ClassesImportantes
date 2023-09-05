07ebb892f57b0d04b355b211de617b174bf870c4
felipricardo <felipebarbosaok@gmail.com>
2023-09-05 16:07:42 -0300

    up

--- TaskConsole/Program.cs
+++ TaskConsole/Program.cs
@@ -10,33 +10,58 @@ namespace TaskConsole
     {
         static void Main(string[] args)
         {
+            #region Task Parte 1
+            // Criando uma tarefa usando Task construtor e, em seguida, iniciando-a
             //Task t1 = new Task(Tarefa);
             //t1.Start();
 
+            // Criando uma tarefa usando Task.Run e iniciando-a (forma mais comum)
             //Task t2 = Task.Run(Tarefa);
 
+            // Criando e iniciando uma tarefa diretamente com Task.Run
             //Task.Run(Tarefa);
 
-            Task.Factory.StartNew(Tarefa);
+            // Criando e iniciando uma tarefa usando Task.Factory.StartNew (menos comum)
+            //Task.Factory.StartNew(Tarefa);
 
-            Task.Run(() => 
+            // Criando e iniciando uma tarefa anônima com Task.Run
+            //Task.Run(() =>
+            //{
+            //    for (int i = 0; i < 10; i++)
+            //    {
+            //        Console.WriteLine("Tarefa anônima.");
+            //    }
+            //});
+
+            // Loop principal
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    Console.WriteLine("Principal.");
+            //}
+            #endregion
+
+            Task[] tasks = 
             {
-                for (int i = 0; i < 10; i++)
+                Task.Factory.StartNew(() => 
                 {
-                    Console.WriteLine("Tarefa anônima.");
-                }
-            });
-
-            for (int i = 0; i < 10; i++)
-            {
-                Console.WriteLine("Principal.");
-            }
+                    Console.WriteLine("Tarefa #1");
+                }),
+                Task.Factory.StartNew(() => 
+                {
+                    Console.WriteLine("Tarefa #2");
+                }),
+                Task.Factory.StartNew(() => 
+                {
+                    Console.WriteLine("Tarefa #3");
+                })
+            };
 
             Console.ReadKey();
         }
 
         static private void Tarefa()
         {
+            // Tarefa que imprime "Tarefa do task." 10 vezes
             for (int i = 0; i < 10; i++)
             {
                 Console.WriteLine("Tarefa do task.");
