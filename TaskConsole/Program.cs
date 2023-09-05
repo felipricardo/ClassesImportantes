63094d4bbea85c27aa296e2dc632c69e7768f5e8
felipricardo <felipebarbosaok@gmail.com>
2023-09-05 15:28:52 -0300

    up

--- TaskConsole/Program.cs
+++ TaskConsole/Program.cs
@@ -10,16 +10,22 @@ namespace TaskConsole
     {
         static void Main(string[] args)
         {
-            Task t1 = new Task(Tarefa);
-            t1.Start();
+            //Task t1 = new Task(Tarefa);
+            //t1.Start();
 
-            Task t2 = Task.Run(Tarefa);
+            //Task t2 = Task.Run(Tarefa);
 
-            Task.Run(Tarefa);
+            //Task.Run(Tarefa);
 
             Task.Factory.StartNew(Tarefa);
 
-            Task.Run(() => { });
+            Task.Run(() => 
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    Console.WriteLine("Tarefa do task.");
+                }
+            });
         }
 
         static private void Tarefa()
