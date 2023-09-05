30194ebabece1bb2887fcb3ba9dcc49708266024
felipricardo <felipebarbosaok@gmail.com>
2023-09-05 15:05:17 -0300

    up

--- ThreadForm/Form1.cs
+++ ThreadForm/Form1.cs
@@ -14,27 +14,28 @@ namespace ThreadForm
 {
     public partial class Form1 : Form
     {
-        private delegate void AtualizarControle(Control controle, string propridade, object valor);
+        // Delegado para atualizar controles da interface gráfica de forma segura
+        private delegate void AtualizarControle(Control controle, string propriedade, object valor);
 
-        Thread t;
-        
+        Thread t; // Declaração da thread
+
         public Form1()
         {
             InitializeComponent();
             t = new Thread(new ThreadStart(Tarefa));
-            t.IsBackground = true;
+            t.IsBackground = true; // Define a thread como um thread em segundo plano
         }
 
         private void btnPrincipal_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("Principal");
+            MessageBox.Show("Principal"); // Exibe uma mensagem de caixa de diálogo
         }
 
         private void btnContador_Click(object sender, EventArgs e)
         {
-            if (!t.IsAlive)
+            if (!t.IsAlive) // Verifica se a thread está em execução
             {
-                t.Start();
+                t.Start(); // Inicia a thread se não estiver em execução
             }
         }
 
@@ -42,22 +43,24 @@ namespace ThreadForm
         {
             while (true)
             {
-                //lblResultado.Text = DateTime.Now.Second.ToString();
-                DefinirValorPropriedade(lblResultado, "Text", DateTime.Now.Second.ToString());
+                //lblResultado.Text = DateTime.Now.Second.ToString(); // Atualiza o texto de um controle no formulário
+                DefinirValorPropriedade(lblResultado, "Text", DateTime.Now.Second.ToString()); // Chama o método para atualizar a propriedade de controle
             }
 
-            //DefinirValorPropriedade(lblResultado, "BackColor", Color.Orange);
+            //DefinirValorPropriedade(lblResultado, "BackColor", Color.Orange); // Define a cor de fundo de um controle
         }
 
         private void DefinirValorPropriedade(Control controle, string propriedade, object valor)
         {
             if (controle.InvokeRequired)
             {
+                // Se a chamada não está no thread de interface, invoque o método de forma segura no thread de interface
                 AtualizarControle d = new AtualizarControle(DefinirValorPropriedade);
-                controle.Invoke(d, new object[] {controle, propriedade, valor});
+                controle.Invoke(d, new object[] { controle, propriedade, valor });
             }
             else
             {
+                // Caso contrário, atualize a propriedade do controle diretamente
                 Type t = controle.GetType();
                 PropertyInfo[] props = t.GetProperties();
 
