6cf7454b25e09616ff5499d7269a31f9c3c76804
felipricardo <felipebarbosaok@gmail.com>
2023-09-05 15:18:42 -0300

    up

---
+++ TaskConsole/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        private void Tarefa()
+        {
+            for (int i =0; i < 10; i++)
+            {
+                Console.WriteLine("Tarefa do task.");
+            }
+        }
+    }
+}
