dd7ab642f46aa2def54e36f110e0b3f6721306a2
felipricardo <felipebarbosaok@gmail.com>
2023-09-05 16:23:04 -0300

    up

--- TaskConsole/Program.cs
+++ TaskConsole/Program.cs
@@ -40,27 +40,32 @@ namespace TaskConsole
             //}
             #endregion
 
-            Task[] tasks =
-            {
-                Task.Factory.StartNew(() =>
-                {
-                    Console.WriteLine("Tarefa #1");
-                }),
+            //Task[] tasks =
+            //{
+            //    Task.Factory.StartNew(() =>
+            //    {
+            //        Console.WriteLine("Tarefa #1");
+            //    }),
 
-                Task.Factory.StartNew(() => 
-                {
-                    Console.WriteLine("Tarefa #2");
-                }),
+            //    Task.Factory.StartNew(() => 
+            //    {
+            //        Console.WriteLine("Tarefa #2");
+            //    }),
 
-                Task.Factory.StartNew(() => 
-                {
-                    Console.WriteLine("Tarefa #3");
-                })
-            };
+            //    Task.Factory.StartNew(() => 
+            //    {
+            //        Console.WriteLine("Tarefa #3");
+            //    })
+            //};
 
-            Task.WaitAll(tasks);
+            //Task.WaitAll(tasks);
 
-            Console.WriteLine("Principal");
+            Task t1 = Task.Run(() => { Console.WriteLine("Comando #1"); });
+            Task t2 = Task.Run(() => { Console.WriteLine("Comando #2"); });
+            Task t3 = Task.Run(() => { Console.WriteLine("Comando #3"); });
+            Task.WaitAll(t1, t2, t3);
+
+            Console.WriteLine("Principal.");
 
             Console.ReadKey();
         }
